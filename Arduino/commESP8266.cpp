582b3f79e00f34afc68cd3ada5b8d8c825c3a2a2
TheSirC <clocloranely@hotmail.fr>
2016-04-08 21:26:58 +0200

    Ajout de commande dans le code de l'Arduino
    Ajout de code permettant l'initialisation du serveur Web sur la Teensy
    (Arduino)

--- Arduino/commESP8266.cpp
+++ Arduino/commESP8266.cpp
@@ -27,12 +27,56 @@ void setup()
   sendData("AT+CWMODE=2\r\n", DEBUG); // 2 configure comme un point d'accès
   sendData("AT+CIPMUX=1\r\n", DEBUG); // // Permet les connexions multiples
   sendData("AT+CIPSERVER=1,80\r\n", DEBUG); // Mise en route du serveur en port 80
-  sendData("AT+CWSAP?\r\n", DEBUG); // Retourne le SSID, le mot de passe, le channel d'émission et le cryptage de l'AP produite
+  sendData("AT+CWSAP?\r\n", DEBUG); // Retourne le SSID, le mot de passe, le cannal d'émission et le cryptage de l'AP produite
+  sendData("AT+CIFSR\r\n",DEBUG); // Retourne l'adresse IP de la passerelle associée au ESP8266
+
 }
 
 void loop()
 {
+  //Serveur http :
 
+  if(esp8266.available()) // On vérifie que l'ESP n'envoie pas de message
+  {
+
+    if(esp8266.find("+IPD,")) // Si on a une connexion
+    {
+      delay(1000);
+
+      int connectionId = esp8266.read()-48; //On soustrait 48 car read() retourne la valeur décimal
+      // en ASCII et 0 (le premier chiffre décimal) commence à 48.
+
+      //ConnectionId contient l'Id de la connexion
+
+      String webpage = "<.h1>Hello<.h1><.h2>World!</h2><button>LED1</button>";
+
+      // On défini la taille de la commande qui va être envoyée
+      String cipSend = "AT+CIPSEND=" cipSend += connectionId;
+      cipSend += ",";
+      cipSend +=webpage.length();
+      cipSend +="\r\n";
+
+      sendData(cipSend,DEBUG);
+      sendData(webpage,DEBUG);
+
+      webpage="LED2";
+
+      cipSend = "AT+CIPSEND=";
+      cipSend += connectionId;
+      cipSend += ",";
+      cipSend +=webpage.length();
+      cipSend +="\r\n";
+
+      sendData(cipSend,DEBUG);
+      sendData(webpage,DEBUG);
+
+      //Fermeture de la connexion
+      String closeCommand = "AT+CIPCLOSE=";
+      closeCommand+=connectionId; // append connection id
+      closeCommand+="\r\n"
+
+      sendData(closeCommand,DEBUG);
+    }
 }
 
 void sendData(String command, bool debug)
