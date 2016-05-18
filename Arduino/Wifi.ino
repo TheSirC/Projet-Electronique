cbcdb4de33e97cbba8bbae3320ad0fcc3e2d9e9c
Chamoniix <hugo4242@hotmail.fr>
2016-05-18 16:26:06 +0200

    Ajout Asservissement moteurs
    Première version entièrement fonctionnelle du projet : robot télécommandé par WIFI

--- Arduino/Wifi.ino
+++ Arduino/Wifi.ino
@@ -2,16 +2,264 @@
 
 #include<String>
 
-#define DEBUG true
+#include <TimerOne.h>
+#include <time.h>
+#include <Wire.h>
+
+
 #define esp8266 Serial1
 #define WifiOn 4
 #define LedPin 13
 
+#define DEBUG 0
 
-float consigneVitesseM1 = 0;  // vitesse demand?e
+
+
+const int ledPin = 13;  // Teensy 3.0 has the LED on pin 13
+int dObstacle = 10;
+
+
+// sorties  de controle des moteurs
+const int M2Enable = 23;
+const int M1Enable = 22;
+const int M2Out1 = 16;
+const int M2Out0 = 17;
+const int M1Out1 = 14;
+const int M1Out0 = 15;
+// entr�e capteur
+const int M1In1 = 5;
+const int M1In0 = 6;
+const int M2In1 = 8;  // sens inverse de M1
+const int M2In0 = 7;
+
+
+// interruption sur M1In0
+//variables globales
+volatile long int posM1 = 0;   // volatile pour modif dans l'interruption  multi processus
+volatile long int posM2 = 0;   // position sur 32 bits
+volatile unsigned long int timeout=0;   // au cas où
+volatile long int posM1Anc = 0;   // volatile pour modif dans l'interruption  multi processus
+volatile long int posM2Anc = 0;   // position pr�c�dente pour le calcul de la vitesse
+volatile float vM1 = 0;   // vitesse en pas par seconde
+volatile float vM2 = 0;
+int intervalAsserv = 20000;  // 50000�s entre chaque interruption du timer 50ms
+float intervalAsservSec = (float)intervalAsserv / 1.0E+6;
+volatile float fAux1 = 0;  // variable de retour
+volatile float fAux2 = 0;
+
+// coeffiscient de correction de puissance
+float kPM1 = 2.5;
+float kPM2 = 2.5;
+
+float consigneVitesseM1 = 0;  // vitesse demand�e
 float consigneVitesseM2 = 0;
-// liste des param?tres globaux
-float paramVitesse = 1000;   //  
+// liste des param�tres globaux
+float paramVitesse = 1000;   // 
+
+
+// interruption declench�e par la fourche optique du moteur 1
+void ISRM1()
+{   noInterrupts();  // zone ex
+
+   // tester M1In0
+    if (digitalRead( M1In0) == HIGH)
+    posM1++;  // dans sens positif voir chronogramme
+  else
+    posM1--;  // dans sens n�gatif
+  
+  
+  interrupts();  // zone ex
+  
+}
+// interruption declench�e par la fourche optique du moteur 2
+void ISRM2()
+{
+  noInterrupts();  // zone ex
+  // tester M1In0
+  if (digitalRead( M2In0) == HIGH)
+    posM2++;  // dans sens positif voir chronogramme
+  else
+    posM2--;
+  
+  interrupts();  // zone ex
+  
+}
+// fonction simple de calcul de la puissance en fonction de l'erreur sur la vitesse
+int fonctionP( float v, float consigne, float k)
+{
+  if (fabs(consigne ) <10)
+    return 0;  // c'est un arret
+
+  float erreur = consigne - v;
+  float puissance = k * erreur;
+  fAux1 = erreur;
+  fAux2 = puissance;
+
+  if ( fabs(puissance) < 10)
+    return 0;  // arret
+  else if ( puissance < -255)
+    return -255;
+  else if ( puissance > 255)
+    return 255;
+  else
+    return (int)puissance;
+}
+// fonctions d'asservissement
+void regulV()
+{
+    
+  MotorCC1( fonctionP( vM1, consigneVitesseM1, kPM1));  // calcul de puissance et application
+  MotorCC2( fonctionP( vM2, consigneVitesseM2, kPM2));
+}
+// interruption du timer : calcul de la vitesse et asservissement
+void ITimer1()
+{
+  if (timeout > 0)
+    timeout--;
+  // calcul de vitesse
+  vM1 = (float)(posM1 - posM1Anc) / intervalAsservSec;
+  vM2 = (float)(posM2 - posM2Anc) / intervalAsservSec;
+  posM1Anc = posM1;
+  posM2Anc = posM2;
+  // appel fonctions d'asservissement
+  regulV();
+
+}
+
+// fonction bas niveau de commande des moteurs
+void MotorCC1( bool bEnable, bool bSens)
+{
+  if ( bSens)
+  {
+    digitalWrite( M1Out0, HIGH);    // rive gauche du pont � Vc
+    digitalWrite( M1Out1, LOW);    // rive droite du pont � 0
+  }
+  else
+  {
+    digitalWrite( M1Out0, LOW);    // rive gauche du pont � 0
+    digitalWrite( M1Out1, HIGH);    // rive droite du pont � Vc
+  }
+
+  
+  digitalWrite( M1Enable, bEnable?HIGH:LOW);    // autoriser ou non
+  
+}
+void MotorCC1( int iPuissance, bool bSens)
+{
+  if ( bSens)
+  {
+    digitalWrite( M1Out0, HIGH);    // rive gauche du pont � Vc
+    digitalWrite( M1Out1, LOW);    // rive droite du pont � 0
+  }
+  else
+  {
+    digitalWrite( M1Out0, LOW);    // rive gauche du pont � 0
+    digitalWrite( M1Out1, HIGH);    // rive droite du pont � Vc
+  }
+
+  analogWrite( M1Enable, iPuissance);
+
+}
+void MotorCC2( int iPuissance, bool bSens)
+{
+  if ( bSens)
+  {
+    digitalWrite( M2Out0, HIGH);    // rive gauche du pont � Vc
+    digitalWrite( M2Out1, LOW);    // rive droite du pont � 0
+  }
+  else
+  {
+    digitalWrite( M2Out0, LOW);    // rive gauche du pont � 0
+    digitalWrite( M2Out1, HIGH);    // rive droite du pont � Vc
+  }
+
+  analogWrite( M2Enable, iPuissance);
+  
+}
+void MotorCC1( int iPuissance)
+{
+  if ( iPuissance>=0)
+  {
+    digitalWrite( M1Out0, HIGH);    // rive gauche du pont � Vc
+    digitalWrite( M1Out1, LOW);    // rive droite du pont � 0
+  }
+  else
+  {   // sens n�gatif
+    iPuissance = -iPuissance;
+    digitalWrite( M1Out0, LOW);    // rive gauche du pont � 0
+    digitalWrite( M1Out1, HIGH);    // rive droite du pont � Vc
+  }
+
+  analogWrite( M1Enable, iPuissance);  // 
+  
+}
+void MotorCC2( int iPuissance)
+{
+  if ( iPuissance>=0)
+  {
+    digitalWrite( M2Out0, HIGH);    // rive gauche du pont � Vc
+    digitalWrite( M2Out1, LOW);    // rive droite du pont � 0
+  }
+  else
+  {   // sens n�gatif
+    iPuissance = -iPuissance;
+    digitalWrite( M2Out0, LOW);    // rive gauche du pont � 0
+    digitalWrite( M2Out1, HIGH);    // rive droite du pont � Vc
+  }
+
+  analogWrite( M2Enable, iPuissance);  
+
+}
+// divers
+void Stop()
+{
+  MotorCC1( 0, true);  
+  MotorCC2( 0, true);  
+}
+
+void posOrigine()
+{
+  posM1 = 0;
+  posM2 = 0;
+  
+  digitalWrite(ledPin, LOW);    // no error
+}
+
+// liaison bluetooth
+String readHC06( )
+{
+  String sRet;
+  
+  sRet = Serial1.readString( ); // echo + r�ponse
+#ifdef DEBUG
+  Serial.println( "r->" + sRet);    // debug
+#endif
+  return sRet;
+}
+
+String sendHC06( String as)
+{
+  String sRet;
+  Serial1.print( as);
+  
+  sRet = Serial1.readString( ); // echo + r�ponse
+#ifdef DEBUG
+  Serial.println( "r->" + sRet);    // debug
+#endif
+  return sRet;
+}
+
+String sendHC06_ln( String as)
+{
+  String sRet;
+  Serial1.println( as);
+  
+  sRet = Serial1.readString( ); // echo + r�ponse
+#ifdef DEBUG
+  Serial.println( "r->" + sRet);    // debug
+#endif
+  return sRet;
+}
 
 
 //String NomduReseauWifi = "HugoSpot"
@@ -24,10 +272,43 @@ void setup()
   pinMode(LedPin, OUTPUT);
   digitalWrite(WifiOn, HIGH);
   
+  // interruption
+  pinMode( M1In1, INPUT); // inter
+  pinMode( M1In0, INPUT); // quadrature
+  attachInterrupt( M1In1, ISRM1, FALLING);  // li� l'�v�nement interruption sur le pin M1In1 avec la fonction ISRM1
+  // moteur 2
+  pinMode( M2In1, INPUT); // inter
+  pinMode( M2In0, INPUT); // quadrature
+  attachInterrupt( M2In1, ISRM2, FALLING);  // li� l'�v�nement interruption sur le pin M1In1 avec la fonction ISRM1
+
+   pinMode( M1Enable, OUTPUT);  
+  pinMode( M2Enable, OUTPUT);  
+  pinMode( M1Out0, OUTPUT);  
+  pinMode( M1Out1, OUTPUT);  
+  pinMode( M2Out0, OUTPUT);  
+  pinMode( M2Out1, OUTPUT);  
+
+   // timer pour l'asservissement
+  Timer1.initialize(intervalAsserv); // choix du delais
+  Timer1.attachInterrupt( ITimer1);  // accroche de la routine d'interruption
+  
+  // liaisons s�ries
+
+  
+
+  // sortie
+    pinMode( M1Enable, OUTPUT);   
+  #ifdef DEBUG
+    Serial.begin( 115200); // debug avec la console
+    delay( 100);
+    Serial.println( "init...");
+  #endif
+    
   //Démarrage des communications séries:
   Serial.begin(9600); // On démarre la communication avec le PC
   esp8266.begin(9600); // On démare la communication en l'Arduino et l'ESP
 
+  digitalWrite(ledPin, HIGH); 
   
   delay(2000); // Temps d'attente pour que l'initialisation se déroule correctement
 
@@ -47,6 +328,12 @@ void setup()
   //sendData("AT+CIPSTO=5000\r\n",DEBUG);
   //sendData("\r\n",DEBUG);
 
+  esp8266.setTimeout(100);
+  digitalWrite(ledPin, LOW); 
+  delay(50);
+  digitalWrite(ledPin, HIGH); 
+  delay(50);
+  digitalWrite(ledPin, LOW); 
 }
 
 void loop()
@@ -81,13 +368,13 @@ void loop()
       
       String cmd;
       
-      String webpage = "<h1>Hello<h1><h2>World!</h2><button>LED1</button>";
+      //String webpage = "<h1>Hello<h1><h2>World!</h2><button>LED1</button>";
 
       // On défini la taille de la commande qui va être envoyée
       String cipSend = "AT+CIPSEND=";
       cipSend += connectionId;
-      cipSend += ",";
-      cipSend +=webpage.length();
+      //cipSend += ",";
+      //cipSend +=webpage.length();
       cipSend +="\r\n";
 
       
@@ -100,8 +387,14 @@ void loop()
       {
         //Serial.println("#####BAS !!####"); 
         //BlindLed(100);
+
+        //consigneVitesseM1 = -paramVitesse;
+        //<consigneVitesseM2 = -paramVitesse; 
+
+        consigneVitesseM1 = 0;
+        consigneVitesseM2 = 0;
         
-        digitalWrite(ledPin, HIGH);
+        //digitalWrite(ledPin, HIGH);
       }
 
 
@@ -110,8 +403,12 @@ void loop()
       {
         //Serial.println("####DROITE !!####"); 
         //BlindLed(200);
+
+        consigneVitesseM1 = paramVitesse/2;
+        consigneVitesseM2 = paramVitesse;
         
-        digitalWrite(ledPin, HIGH);
+        
+        //digitalWrite(ledPin, HIGH);
       }
 
 
@@ -120,8 +417,11 @@ void loop()
       {
         //Serial.println("####GAUCHE !!####"); 
         //BlindLed(300);
+
+        consigneVitesseM1 = paramVitesse;
+        consigneVitesseM2 = paramVitesse/2;
         
-        digitalWrite(ledPin, HIGH);
+        //digitalWrite(ledPin, HIGH);
       }
 
 
@@ -130,15 +430,28 @@ void loop()
       {
         //Serial.println("####HAUT !!####"); 
         //BlindLed(400);
+        consigneVitesseM1 = paramVitesse;
+        consigneVitesseM2 = paramVitesse;
+        //digitalWrite(ledPin, HIGH);
+      }
+
+
+      if(cmd == ",5:S80;;")
+      {
+        //Serial.println("####STOP !!####"); 
+        //BlindLed(200);
+
+        consigneVitesseM1 = 0;
+        consigneVitesseM2 = 0;
         
-        digitalWrite(ledPin, HIGH);
+        
+        //digitalWrite(ledPin, HIGH);
       }
 
 
 
-
       
-      webpage="LED2";
+      /*webpage="LED2";
 
       cipSend = "AT+CIPSEND=";
       cipSend += connectionId;
@@ -152,7 +465,7 @@ void loop()
       //Fermeture de la connexion
       String closeCommand = "AT+CIPCLOSE=";
       closeCommand+=connectionId; // append connection id
-      closeCommand+="\r\n";
+      closeCommand+="\r\n";*/
 
       //sendData(closeCommand,DEBUG);
 
@@ -191,4 +504,5 @@ void BlindLed(int time)
     delay(time);
   }
 }
+
 
